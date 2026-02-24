70c6f57ab4a888ca212a1e22348a5adf0d122cbe
PriyansRaj <230701403@rajalakshmi.edu.in>
2026-02-24 23:08:37 +0530

    Assignment 2 Temperature Converter

--- techlounge2/app/src/main/java/com/example/techlounge/MainActivity.kt
+++ techlounge2/app/src/main/java/com/example/techlounge/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.techlounge
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,20 +13,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        var count = 0
-        val stdNo = findViewById<TextView>(R.id.stdNo)
-        val checkIn = findViewById<Button>(R.id.checkIn)
-        val checkOut = findViewById<Button>(R.id.checkOut)
-        checkIn.setOnClickListener {
-            count++
-            stdNo.setText(count.toString())
-        }
-        checkOut.setOnClickListener {
-            count--
-            if (count <=0) {
-                count = 0
+
+        val etName = findViewById<EditText>(R.id.etName)
+        val etMarks = findViewById<EditText>(R.id.etMarks)
+        val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        btnCalculate.setOnClickListener {
+            val name = etName.text.toString()
+            val mark = etMarks.text.toString()
+            if (name.isEmpty() || mark.isEmpty()){
+                tvResult.text = "Please fill all fields"
+                return@setOnClickListener
             }
-            stdNo.setText(count.toString())
+
+            val marks = mark.toInt()
+            if (marks !in 0..100) {
+                etMarks.error = "Marks must be 0-100"
+                return@setOnClickListener
+            }
+            val grade = when{
+                marks >=90-> "A+"
+                marks >=80->"A"
+                marks >=70 ->"B+"
+                marks >=60 ->"B"
+                marks >=50 ->"C"
+                else -> "Fail"
+
+            }
+            val result = if(marks >=50) "Pass" else "Fail"
+            tvResult.text = """
+                Name: $name
+                Marks: $marks
+                Result: $result
+                Grade: $grade
+            """.trimIndent()
+
         }
+
     }
 }
