f7de0dec82bb4c6ae9bdc35cff00c7fd2ae321df
PriyansRaj <230701403@rajalakshmi.edu.in>
2026-02-25 21:22:29 +0530

    Added 2 projects

---
+++ Experiment1/app/src/main/java/com/example/experiment1/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.experiment1
+
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var tvTitle = findViewById<TextView>(R.id.Title)
+
+    }
+}
